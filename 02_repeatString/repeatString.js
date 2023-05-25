75c7c2ee8b7d8bb80c407b1f88aa893bfb904dd7
Philip Freame <freame.p@gmail.com>
2023-05-25 15:27:59 +0200

    finished javascript-exercises 01+02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  if (num < 0) {
+    return "ERROR";
+  }
+  let concatString = "";
+  for (i = 0; i < num; i++) {
+    concatString += string;
+  }
+  return concatString;
 };
 
 // Do not edit below this line
