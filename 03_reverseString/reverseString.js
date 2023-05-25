f38d6294a6f4f041195d11e901a21f66284c4bbe
Philip Freame <freame.p@gmail.com>
2023-05-25 20:57:53 +0200

    finished 03 and 04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function (myString) {
+  //   let splittet = myString.split("");
+  //   let reversed = splittet.reverse();
+  //   let joinedArr = reversed.join("");  .join() default would be , seperatet
 
+  return myString.split("").reverse().join("");
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
-
+// the spread operator ...args puts all input numbers into an array.
+const removeFromArray = function (arr, ...args) {
+  //copy the array
+  let newArr = [...arr];
+  // for of uses values  and the for in uses keys
+  for (arg of args) {
+    let removeIndex = newArr.indexOf(arg); //find the index with value
+    //if not found it returns -1
+    if (removeIndex !== -1) {
+      //splice modifies the arr
+      newArr.splice(removeIndex, 1); //remove 1 item at index. we dont need the removed value so we dont assign this to a new variable
+    }
+  }
+  return newArr;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+// Arr1[1,2,3,4,5]
