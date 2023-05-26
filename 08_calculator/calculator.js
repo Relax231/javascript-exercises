9a1137a9b684c9879ac1f3d875d500621d52f46d
Relax231 <freame.p@gmail.com>
2023-05-26 16:40:52 +0200

    working on calculator: ToDO power

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,28 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let array = [...arr];
+  return array.reduce((summe, current) => summe + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  let array = [...arr];
+  let result = array[0];
+  for (let i = 1; i < array.length; i++) {
+    result *= array[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
-};
+const power = function () {};
 
-const factorial = function() {
-	
-};
+const factorial = function () {};
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +31,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
