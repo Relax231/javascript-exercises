0875a22b56d1936cfec8ced9651686ef54e4c089
Relax231 <freame.p@gmail.com>
2023-05-26 20:23:42 +0200

    finished problem 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -25,9 +25,17 @@ const multiply = function (arr) {
   // return result;
 };
 
-const power = function () {};
+const power = function (base, exponent) {
+  return Math.pow(base, exponent);
+};
 
-const factorial = function () {};
+const factorial = function (n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+};
 
 // Do not edit below this line
 module.exports = {
