607a6ac7074b9217d0f9762814a5ac8cc4ebba39
Relax231 <freame.p@gmail.com>
2023-05-26 21:46:55 +0200

    finished 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+// const palindromes = function (string) {
+//   let bereinigt = string.replace(/[^a-zA-Z]/g, "").toLowerCase();
+//   const array = [...bereinigt];
+//   const reversed = [...array].reverse(); //... or slice damit kopie und nicht reference
+//   let isPalindrome = true;
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] !== reversed[i]) {
+//       isPalindrome = false;
+//       break;
+//     }
+//   }
+//   return isPalindrome;
+// };
 
+const palindromes = function (string) {
+  let bereinigt = string.replace(/[^a-zA-Z]/g, "").toLowerCase();
+  return bereinigt == bereinigt.split("").reverse().join("");
 };
 
 // Do not edit below this line
